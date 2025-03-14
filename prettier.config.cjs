4fb72e760864dc8647f29f7ad46577af29c75f1f
Ramiz <63073421+ramizwd@users.noreply.github.com>
2025-03-14 14:30:38 +0200

    layout and navigation

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+
+const options = {
+  arrowParents: 'always',
+  bracketSpacing: true,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+};
+
+module.exports = options;
